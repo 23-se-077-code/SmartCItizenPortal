5ef021b5b397e02c5eaf198879d52050ab0234d0
Azdar Ali <23-se-077@student.hitecuni.edu.pk>
2026-06-28 10:09:53 +0500

    Fixed Vercel configuration for Django

--- SmartCitizenPortal/smart_portal/wsgi.py
+++ SmartCitizenPortal/smart_portal/wsgi.py
@@ -1,4 +1,9 @@
 ﻿import os
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_portal.settings')
+
 application = get_wsgi_application()
+
+# Vercel requires this
+app = application
